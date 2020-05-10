369a37bf53df1c863538d5a45ae3b52dd5848b66
PauloRenato98 <paulo__159@hotmail.com>
2020-05-10 02:05:49 -0300

    Ajeitando a estrutura

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import './App.css';
-
-import { Button } from 'reactstrap';
-
-
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1> TESTE </h1>
-      <Button color="danger">Danger!</Button>
-      </header>
-    </div>
+    <h1>teste</h1>
   );
 }
 
---
+++ src/pages/login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import { Button } from 'reactstrap';
+
+
+
+function login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1> TESTE </h1>
+      <Button color="danger">Danger!</Button>
+      </header>
+    </div>
+  );
+}
+
+export default login;
