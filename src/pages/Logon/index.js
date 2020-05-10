c2bd56143f2bfb96e0f09ff40ddf3d4ca870f705
PauloRenato98 <paulo__159@hotmail.com>
2020-05-10 13:39:01 -0300

    Atualizando paginade login

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import './styles.css'
+
+import Image from 'react-bootstrap/Image'
 
 import ifomeLogo from '../../assets/ifome.jpg'
 
 
 export default function Logon(){
     return(
-        <div>
-            <section className="logon-container">
-            <img src={ifomeLogo}/>
+<div className="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-5 col-lg-3 col-md-4" style={{marginTop : '10%'}}>
+            <div className="logoprincipal">
+            <Image src={ifomeLogo} height="200px" width="200px" alt="logo ifome"/>
+            </div>
+            <div>
+                <h2>Bem vindo ao Ifome</h2>
+            </div>
+            <div>    
             <form>
-            
             <input placeholder="Seu usuário"/>
+            <input placeholder="Sua senha"/>
+            <div>
+            <button className="buttonentrar" type="submit"> Entrar </button>
             <br/>
-            <input placeholder="Sua Senha"/>
+            <button className="buttongoogle" type="submit"> Login com o Google </button>
+            </div>
+            <a href="/register">Crie sua conta</a>
             </form>
-            <button> Entrar</button>
-            <br/>
-            <button> Login com o Google</button>
-            </section>
-             <a href="/register">Crie sua conta</a>
-
+            
+            </div>
         </div>
+    </div>  
+</div>
     );
 }
+
+
+
+
