f7739bc8fd76313d6efc7a2fb43c3ff73de44795
PauloRenato98 <paulo__159@hotmail.com>
2020-05-11 02:06:36 -0300

    Atualizando css login

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -10,26 +10,25 @@ export default function Logon(){
     return(
 <div className="container">
     <div class="row justify-content-center">
-        <div class="col-xl-5 col-lg-3 col-md-4" style={{marginTop : '10%'}}>
+        <div className="col-xl-5 col-lg-3 col-md-4" style={{marginTop : '10%'}} >
             <div className="logoprincipal">
-            <Image src={ifomeLogo} height="200px" width="200px" alt="logo ifome"/>
+            <Image src={ifomeLogo} height="200px" width="200px" alt="logo ifome" roundedCircle thumbnail/>
             </div>
             <div>
                 <h2>Bem vindo ao Ifome</h2>
             </div>
-            <div>    
-            <form>
-            <input placeholder="Seu usuário"/>
-            <input placeholder="Sua senha"/>
+            <form className="box" height="200px" width="200px">
+            <input id="name" placeholder="Seu usuário"/>
+            <input id="name" type="password" placeholder="Sua senha"/>
             <div>
             <button className="buttonentrar" type="submit"> Entrar </button>
             <br/>
             <button className="buttongoogle" type="submit"> Login com o Google </button>
             </div>
+            <div style={{marginTop : '10%'}}>
             <a href="/register">Crie sua conta</a>
-            </form>
-            
             </div>
+            </form>
         </div>
     </div>  
 </div>
